32fc4b70664441a8b0d46a12a3e94ddd78af21eb
uwoer <handegang@eagleholdings.com.cn>
2018-06-29 10:03:34 +0800

    module 按照新的语法进行编写

--- webpack.config.js
+++ webpack.config.js
@@ -4,71 +4,82 @@ var path = require('path');
 // var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
 module.exports = {
-    //插件项
-    // plugins: [commonsPlugin],
-    //页面入口文件配置
-    entry: {
-        index : './src/js/index.js'
-    },
-    //入口文件输出配置
-    output: {
-        path: 'docs/js/',
-        filename: '[name].js'
-    },
-    module: {
-        //加载器配置
-        loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.js$/, loader: 'jsx-loader?harmony' },
-            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader?sourceMap'},
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            { 
-                context: path.join(__dirname, './src/css/pageThemes'),
-                from: '*', 
-                to: '../pageThemes',
-                force: true
-            },
-            { 
-                context: path.join(__dirname, './src/imgs'),
-                from: '*', 
-                to: '../imgs',
-                force: true
-            },
-            { 
-                context: path.join(__dirname, './src/css/themes'),
-                from: '*', 
-                to: '../themes',
-                force: true
-            },
-            { 
-                context: path.join(__dirname, './src'),
-                from: "index.html",
-                to: '../index.html',
-                force: true
-            },
-            { 
-                context: path.join(__dirname, './src'),
-                from: "demo.md",
-                to: '../demo.md',
-                force: true
-            },
-            { 
-                context: path.join(__dirname, './src'),
-                from: "favicon.ico",
-                to: '../favicon.ico',
-                force: true
-            },
-            { 
-                context: path.join(__dirname, './src'),
-                from: "CNAME",
-                to: '../CNAME',
-                toType: 'file',
-                force: true
-            }
-        ])
+  //插件项
+  // plugins: [commonsPlugin],
+  //页面入口文件配置
+  entry: {
+    index : './src/js/index.js'
+  },
+  //入口文件输出配置
+  output: {
+    path:path.resolve(__dirname, 'docs/js'),
+    filename: '[name].js'
+  },
+  module: {
+    //加载器配置
+    rules: [
+      { test: /\.css$/, use: ['style-loader','css-loader'] },
+      { test: /\.js$/, use: [{
+        loader: 'jsx-loader',
+        options:{
+          harmony: true
+        }
+      }]},
+      { test: /\.less$/, use: ['style-loader','css-loader',
+        {loader: 'less-loader',options:{sourceMap: true}}]},
+      { test: /\.(png|jpg)$/, use: [ {
+        loader: 'url-loader',
+        options: {
+          limit: 8192
+        }
+      }]}
     ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        context: path.join(__dirname, './src/css/pageThemes'),
+        from: '*',
+        to: '../pageThemes',
+        force: true
+      },
+      {
+        context: path.join(__dirname, './src/imgs'),
+        from: '*',
+        to: '../imgs',
+        force: true
+      },
+      {
+        context: path.join(__dirname, './src/css/themes'),
+        from: '*',
+        to: '../themes',
+        force: true
+      },
+      {
+        context: path.join(__dirname, './src'),
+        from: "index.html",
+        to: '../index.html',
+        force: true
+      },
+      {
+        context: path.join(__dirname, './src'),
+        from: "demo.md",
+        to: '../demo.md',
+        force: true
+      },
+      {
+        context: path.join(__dirname, './src'),
+        from: "favicon.ico",
+        to: '../favicon.ico',
+        force: true
+      },
+      {
+        context: path.join(__dirname, './src'),
+        from: "CNAME",
+        to: '../CNAME',
+        toType: 'file',
+        force: true
+      }
+    ])
+  ]
 };
